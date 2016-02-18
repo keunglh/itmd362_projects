c616e6fd02e6114fbe2f019efc7eaa66df49323e
keunglh <lkeung@hawk.iit.edu>
2016-02-18 17:29:56 -0600

    Does this work now? Method 3 on trying to get form to clear without getting Error 405.

--- onboarding/submit.js
+++ onboarding/submit.js
@@ -1,8 +1,6 @@
 
 $('#SPAMTHISGUY').submit(function (ev){
-    var email = document.getElementById('email');
-    var name = document.getElementById('name');
     alert('Thank you for giving us permission to steal and sell your medical information!');
-    email.value = "";
-    name.value="";
 });
+
+$('#SPAMTHISGUY').trigger("reset");
